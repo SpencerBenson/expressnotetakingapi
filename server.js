695940126c6af8b837aeb84292fc1d087fb5a686
Spencer Benson <bennspencer@gmail.com>
2020-03-22 14:27:43 +0000

    project entry point file added

--- server.js
+++ server.js
@@ -1,1 +1,42 @@
-console.log('Welcometo the note taking app');
+
+//Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+//connecting to the database.
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser:true
+}).then(() => {
+    console.log('Successfully connected to the DB')
+}).catch(err => {
+    console.log('could not connect to the database. Exiting...'+err);
+    process.exit();
+});
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Create express app
+const app = express();
+
+//parse requests of content type - application/x-www-form-url-endcoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// define a simple route
+app.get('/', (req, res)=> {
+    res.status(200).json({'message':'hello notsey!'});
+});
+
+// Require Notes routes
+require('./app/routes/note.routes.js')(app);
+//listen for requests
+const port = 3001
+app.listen(port,() => {
+    console.log('Server is listening on port: ' + port);
+})
